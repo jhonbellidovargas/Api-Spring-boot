1d01690175bb2f53fc5d19565ca2abade7937d04
Jhon Bellido Vargas <jhon.bellido.27@unsch.edu.pe>
2022-06-05 20:52:15 -0500

    first commit

---
+++ src/main/java/com/jhon/Strivexshop/StrivexShopApplication.java
@@ -0,0 +1,13 @@
+package com.jhon.Strivexshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrivexShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrivexShopApplication.class, args);
+	}
+
+}
